32851765f89a708cff95ce0fba252149df5f52f5
KirillDolyn <kirill.dolinskij@yandex.ru>
2022-01-29 19:57:58 +0300

    добавил адаптацию

--- src/pages/home/components/cards/Card.tsx
+++ src/pages/home/components/cards/Card.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
-import { Cart } from './Cards';
-import s from './Card.module.scss';
+import React from "react";
+import { GlobalSvgSelector } from "../../../../assets/icons/global/GlobalSvgSelector";
+import { Cart } from "./Cards";
+import s from "./Card.module.scss";
 
 interface Props {
-    carts: Cart,
-};
+  carts: Cart;
+}
 
- const Card = ({carts}: Props) => {
-     const {icon, service, text} = carts;
+const Card = ({ carts }: Props) => {
+  const { icon, service, text } = carts;
   return (
-      <div className={s.entrans}>
-  <div className={s.card}>
-      <div className={s.icon}>
-          <GlobalSvgSelector id={icon}/>
+    <div className={s.entrans}>
+      <div className={s.card}>
+        <div className={s.icon}>
+          <GlobalSvgSelector id={icon} />
+        </div>
+        <div className={s.service}>{service}</div>
+        <div className={s.border}></div>
+        <div className={s.text}>{text}</div>
       </div>
-      <div className={s.service}>{service}</div>
-      <div className={s.border}></div>
-      <div className={s.text}>{text}</div>
-  </div>
-  </div>
-  )
+    </div>
+  );
 };
 
 export default Card;
